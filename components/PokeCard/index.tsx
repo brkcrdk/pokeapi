ab65ca8c891b75843911905987bbbfcd0293c98b
Burak <burak@90pixel.com>
2022-12-05 23:43:37 +0300

    Add pokemoncard component.

---
+++ components/PokeCard/index.tsx
@@ -0,0 +1,11 @@
+function PokeCard() {
+  return (
+    <div className="text-cyan-200">
+      <span className="">#123</span>
+      {/* <img class="card--image" src=${pokemon.image} alt=${pokemon.name} /> */}
+      <h1 className="card--name">pokemon.name</h1>
+      <span className="card--details">pokemon.type</span>
+    </div>
+  );
+}
+export default PokeCard;
